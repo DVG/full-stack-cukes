eb06ce30e7a02c636afb1c08f9336f3536745b39
DVG <devryguy@gmail.com>
2014-11-26 20:09:12 -0500

    Fixing problem

--- config/cukes.rb
+++ config/cukes.rb
@@ -3,7 +3,7 @@ class Cukes
   include ActiveSupport::Configurable
 
   self.configure do |config|
-    config.root = Dir[File.dirname(File.expand_path('../../', __FILE__))].first
+    config.root = Dir[File.dirname(File.expand_path('../', __FILE__))].first
     config.rails_root = File.join(config.root, "backend")
     config.ember_root = File.join(config.root, "frontend")
     config.host = "http://localhost:4200"
